2f5a4470d3c074ec451c3a6d9f0de5419230a1af
melody-EPIS <mramoschal@unsa.edu.pe>
2021-07-31 07:10:40 -0500

    Main modificado

--- Propuesto2/TestListAdyacencia.java
+++ Propuesto2/TestListAdyacencia.java
@@ -1,14 +1,16 @@
 public class TestListAdyacencia {
 	public static void main(String[] args) {
 		System.out.println("Lista de adyacencia");
-		ListaAdyacencia nodos=new ListaAdyacencia(7);
-		nodos.addNodo(0, 1);
-		nodos.addNodo(0, 4);
-		nodos.addNodo(1, 2);
-		nodos.addNodo(1, 3);
-		nodos.addNodo(1, 4);
-		nodos.addNodo(2, 3);
-		nodos.addNodo(3, 4);
+		ListaAdyacencia nodos=new ListaAdyacencia(6);
+		nodos.addNodo(0,1,5);
+		nodos.addNodo(0,5,2);
+		nodos.addNodo(1,2,4);
+		nodos.addNodo(2,3,9);
+		nodos.addNodo(3,4,7);
+		nodos.addNodo(3,5,3);
+		nodos.addNodo(4,0,1);
+		nodos.addNodo(5,2,1);
+		nodos.addNodo(5,4,8);
 		System.out.println(nodos);
 	}
 }
